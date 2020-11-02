dd9ddcf0c5da1547e30901b51f182c9bf064a880
Qian Dong <qdong327@gmail.com>
2020-11-01 20:32:22 -0500

    added heroku credentials

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = my.sql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: process.env.BURGER_MYSQL_PASSWORD,
   database: "burgers_db"
 });
+};
 
 
 connection.connect(function(err) {
